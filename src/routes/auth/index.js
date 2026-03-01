448da7ddc7e823878797671af8bcf31469ede9e8
Shane <shanelibera7@gmail.com>
2026-03-01 18:05:31 +0200

    feat: refresh Route to update access token based on refresh token

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const loginRoute = require('./login');
 const registerRoute = require('./register');
-
+const refreshRoute = require('./refresh');
 
 
 //Routes
 router.use('/register', registerRoute);
 router.use('/login', loginRoute);
+router.use('/refresh', refreshRoute);
 
 
 module.exports = router;
