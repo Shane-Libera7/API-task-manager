0d896cefc7ddf838f54ad2f7e8345123caa91452
Shane <shanelibera7@gmail.com>
2026-03-01 18:28:21 +0200

    feat: logout route and logic to remove refresh token from database

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const loginRoute = require('./login');
 const registerRoute = require('./register');
 const refreshRoute = require('./refresh');
+const logoutRoute = require('./logout');
 
 
 //Routes
 router.use('/register', registerRoute);
 router.use('/login', loginRoute);
 router.use('/refresh', refreshRoute);
+router.use('/logout', logoutRoute);
 
 
 module.exports = router;
---
+++ src/routes/auth/logout.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const db = require('../../db');
+const router = express.Router();
+
+
+//Verify token is in database
+
+async function verifyToken(refreshToken) {
+    const token = await db('refresh_tokens').where('token', refreshToken).first();
+
+   return token ? true : false;
+}
+
+
+router.post('/', async (req,res) => {
+    
+    const { refreshToken } = req.body;
+
+    try{
+
+    const tokenInDb = await verifyToken(refreshToken);
+
+    if(tokenInDb){
+        await db('refresh_tokens').where('token', refreshToken).delete();
+        return res.status(204).send();
+
+    } else{
+        return res.status(400).json({ error: 'Invalid token, logout unsuccessful'});
+    }
+
+
+
+    } catch(e){
+        console.log(e);
+        return res.status(500).json({ error: 'Something went wrong'});
+    }
+
+
+
+})
+
+
+module.exports = router;
