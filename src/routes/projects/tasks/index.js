c816116c639ac4fa802bd09b3aba32095d9d329e
Shane <shanelibera7@gmail.com>
2026-03-04 18:59:51 +0200

    feat: Get one task + patch  a task

--- src/routes/projects/tasks/index.js
+++ src/routes/projects/tasks/index.js
@@ -61,7 +61,7 @@ router.get('/', async (req, res) => {
             return res.status(404).json({ error: 'Project is needed to assign task to it'});
         }
 
-        const tasks = db('tasks').select(['id', 'title', 'description', 'priority', 'due_date', 'completed', 'created_at']).where('project_id', projectId);
+        const tasks = await db('tasks').select(['id', 'title', 'description', 'priority', 'due_date', 'completed', 'created_at']).where('project_id', projectId);
         return res.status(200).json(tasks);
 
 
@@ -71,6 +71,85 @@ router.get('/', async (req, res) => {
         return res.status(500).json({ error: 'Something went wrong' });
     }
 });
+
+
+
+//Get a single Task
+
+router.get('/:id', async (req, res) => {
+    const projectId = req.params.projectId;
+    const userId = req.userId;
+    const taskId = req.params.id;
+
+    try{
+        const valid = await verifyProjectOwnership(projectId, userId);
+        if(!valid){
+            return res.status(404).json({ error: 'Project is needed to assign task to it'});
+        }
+
+        const task = await db('tasks').select(['id', 'title', 'description', 'priority', 'due_date', 'completed', 'created_at']).where({ id: taskId, project_id: projectId}).first();
+
+        if (!task){
+            return res.status(404).json({ error: 'Task not found'});
+        }
+
+        return res.status(200).json(task);
+
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({ error:'Something went wrong'});
+    }
+})
+
+
+
+//Update a task
+
+router.patch('/:id', async (req, res) => {
+    const projectId = req.params.projectId;
+    const userId = req.userId;
+    let update = {};
+    const taskId = req.params.id;
+    try{
+        const valid = await verifyProjectOwnership(projectId, userId);
+        if(!valid){
+            return res.status(404).json({ error: 'Project is needed to assign task to it'});
+        }
+
+        if (req.body.title !== undefined){
+            update.title = req.body.title;
+        } if (req.body.description !== undefined){
+            update.description = req.body.description;
+        } if (req.body.priority !== undefined){
+            update.priority = req.body.priority;
+        } if (req.body.due_date !== undefined){
+            update.due_date = req.body.due_date;
+        } if (req.body.completed !== undefined){
+            update.completed = req.body.completed;
+        }
+       
+
+        if (Object.keys(update).length === 0){
+            return res.status(400).json({ error: 'No fields entered to be updated'});
+        }
+
+        const updatedTask = await db('tasks').where({ id: taskId, project_id: projectId}).update(update).returning(['id', 'title', 'description', 'priority', 'due_date', 'completed', 'created_at']);
+        return res.status(200).json(updatedTask);
+
+
+
+
+    }catch(e){
+        console.log(e);
+        res.status(500).json({ error: 'Something went wrong'});
+    }
+
+})
+
+
+
+
+//
 
 
 
