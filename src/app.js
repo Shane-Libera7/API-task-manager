c18804edec7d5454d836a6fe77cc8fd2d1cd2045
Shane <shanelibera7@gmail.com>
2026-03-11 14:14:04 +0000

    edit: restructure server and app files

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const authRoutes = require('./routes/auth/index');
+const projectRoutes = require('./routes/projects/index');
+const taskRoutes = require('./routes/projects/tasks/index');
+const errorHandler = require('../middleware/errorHandler');
+const app = express();
+require('dotenv').config();
+
+//Middleware
+app.use(express.json());
+
+
+//routes 
+app.get('/health', (req,res) =>{
+    res.status(200).json({ status: 'ok'});
+});
+
+//Authentication
+app.use('/auth', authRoutes);
+
+//Project routes
+app.use('/projects', projectRoutes);
+
+//Task routes
+app.use('/:projectId/tasks', taskRoutes);
+
+//Error Handling
+app.use(errorHandler);
+
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,35 +1,5 @@
-const express = require('express');
-const authRoutes = require('./routes/auth/index');
-const projectRoutes = require('./routes/projects/index');
-const taskRoutes = require('./routes/projects/tasks/index');
-const errorHandler = require('./middleware/errorHandler');
-const app = express();
+const app = require('./app');
 const port = 3000;
-require('dotenv').config();
 
-//Middleware
-app.use(express.json());
-
-//Start Server
 app.listen(port);
-
-//routes 
-app.get('/health', (req,res) =>{
-    res.status(200).json({ status: 'ok'});
-});
-
-//Authentication
-app.use('/auth', authRoutes);
-
-//Project routes
-app.use('/projects', projectRoutes);
-
-//Task routes
-app.use('/:projectId/tasks', taskRoutes);
-
-//Error Handling
-app.use(errorHandler);
-
-
-
 
